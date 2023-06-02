03ec1babb90144f7c174bc06ec09e24474f04f24
Noordeep Kaur <noordeep29oct@gmail.com>
2023-06-02 20:51:52 +0530

    updated all user and books routes

--- routes/books.js
+++ routes/books.js
@@ -85,7 +85,7 @@ router.get('/issued/books',(req,res)=>{
  */
 
 router.post('/',(req,res)=>{
-    const{id,name,surname,email,subscriptionType,subscriptionDate}=req.body;
+    const{id,name,author,genre,price,publisher}=req.body;
     const book=books.find((each)=>each.id===id);
     if(book){
      return res.status(404).json({
@@ -96,10 +96,10 @@ router.post('/',(req,res)=>{
     books.push({
      id,
      name,
-     surname,
-     email,
-     subscriptionType,
-     subscriptionDate,
+     author,
+     genre,
+     price,
+     publisher,
     });
     return res.status(201).json({
      success:true,
@@ -164,5 +164,75 @@ router.delete('/:id',(req,res)=>{
      data:books,
     });
  });
+
+//function
+function getFine(user){
+    const getDateInDays=(data="")=>{
+        let date;
+        if(data===""){
+            date= new Date();
+        }else{
+            date=new Date(data);
+        }
+        let days=Math.floor(date/(1000*60*60*24));
+        return days;
+    };
+    
+    const subscriptionType=(date)=>{
+        if(user.subscriptionType==="Basic"){
+            date=date+90;
+        }else if(subscriptionType==="Standard"){
+            date=date+180;
+        }else if(user.subscriptionType==="Premium"){
+            date=date+365;
+        }
+        return date;
+    };
+    //Subscription expiration calculation
+    // January 1, 1970, UTC. in milliseconds
+    let returnDate = getDateInDays(user.returnDate);
+    let currentDate = getDateInDays();
+    let subscriptionDate=getDateInDays(user.subscriptionDate);
+    let subscriptionExpiration= subscriptionType(subscriptionDate);
+
+    const fine= returnDate<currentDate ? subscriptionExpiration<=currentDate?200:100:0;
+    return fine;
+    //dates in mmddyyyy
+    
+ }
+
+ /**
+ * Route: /books
+ * Method: GET
+ * Description: get all issued books with fine
+ * Access: Public
+ * Parameters: none
+ */
+ router.get('/issued-fine/books',(req,res)=>{
+ const userWithIssuedBooks= users.filter((each)=>{
+    if(each.issuedBook) return each;
+});
+
+const issuedBooks=[];
+
+userWithIssuedBooks.forEach((each)=>{
+    const book=books.find((book)=>book.id===each.issuedBook);
+
+    book.issuedBy=each.name;
+    book.issuedDate=each.issuedDate;
+    book.returnDate=each.returnDate;
+    book.fine=getFine(each);
+
+    issuedBooks.push(book);
+})
+
+if(issuedBooks.length===0)
+  return res.status(404).json({
+    success:false,
+    message:"No books issued yet",
+  })
+  return res.status(200).json({success:true,data:issuedBooks,})
+});
+
 //default export - single export
 module.exports=router;
--- routes/users.js
+++ routes/users.js
@@ -126,8 +126,98 @@ router.delete('/:id',(req,res)=>{
     users.splice(index,1);
     return res.status(201).json({
      success:true,
-     data:Users,
+     data:users,
     });
  });
+
+ /**
+ * Route: /users/subscription-details/:id
+ * Method: GET
+ * Description: get all user subscription details
+ * Access: Public
+ * Parameters: id
+ */
+
+router.get('/subscription-details/:id',(req,res)=>{
+    const{id}=req.params;
+    const user=users.find((each)=>each.id===id);
+    if(!user)
+     return res.status(404).json({
+         success:false,
+         message:"User does not exists",
+     });
+    
+    const getDateInDays=(data="")=>{
+        let date;
+        if(data===""){
+            date= new Date();
+        }else{
+            date=new Date(data);
+        }
+        let days=Math.floor(date/(1000*60*60*24));
+        return days;
+    };
+    
+    const subscriptionType=(date)=>{
+        if(user.subscriptionType==="Basic"){
+            date=date+90;
+        }else if(subscriptionType==="Standard"){
+            date=date+180;
+        }else if(user.subscriptionType==="Premium"){
+            date=date+365;
+        }
+        return date;
+    };
+    //Subscription expiration calculation
+    // January 1, 1970, UTC. in milliseconds
+    let returnDate = getDateInDays(user.returnDate);
+    let currentDate = getDateInDays();
+    let subscriptionDate=getDateInDays(user.subscriptionDate);
+    let subscriptionExpiration= subscriptionType(subscriptionDate);
+
+    const data={
+        ...user,
+        subscriptionExpired: subscriptionExpiration<currentDate,
+        daysLeftForExpiration:
+            subscriptionExpiration<=currentDate?0:subscriptionExpiration-currentDate,
+        fine:
+            returnDate<currentDate 
+                ? subscriptionExpiration<=currentDate
+                    ?200
+                    :100
+                :0,
+    };
+    //dates in mmddyyyy
+    res.status(200).json({
+        success:"true",
+        data,
+    });
+
+ });
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * Description: get all issued books with fine of a user
+ * Access: Public
+ * Parameters: none
+ */
+
+router.get('/:id',(req,res)=>{
+    const{id}=req.params;
+    const user=users.find((each)=>each.id===id);
+    if(!user){
+     return res.status(404).json({
+         success:false,
+         message:"User does not exists",
+     });
+    }
+    const index=books.indexOf(book);
+    books.splice(index,1);
+    return res.status(201).json({
+     success:true,
+     data:books,
+    });
+});
  
  module.exports= router;
